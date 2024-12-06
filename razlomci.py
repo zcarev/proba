b3521b8f3f1c6c5b2ec1ddaaec9b1841709b8fe4
zcarev <zdravko.carev@gmail.com>
2024-12-06 23:58:36 +0100

    Update razlomci.py

--- razlomci.py
+++ razlomci.py
@@ -1,31 +1,16 @@
 import streamlit as st
 
-ss = r'''\frac{1}{2}+\frac{2}{3}=\frac{x}{y}'''
-
-st.header('Rješite razlomak:')
-
-cols = st.columns(2)
-
-cols1 = st.columns(2)
-with cols[1]:
-    with cols1[0]:
-        rjx = st.number_input('Unesite brojnik', value=None, step=1)
-        if rjx == 5:
-            st.write('Bravo! - Točan brojnik')
-        else:
-            st.write('Unesite točan odgovor za brojnik')
-
-with cols[1]:
-    with cols1[1]:
-        rjy = st.number_input('Unesite rješenje za nazivnik', value=None, step=1)
-        if rjy == 4:
-            st.write('Bravo! - Točan odgovor za nazivnik')
-        else:
-            st.write('Unesite točan odgovor za nazivnik')
+def zbroj_razlomaka(zamjene:dict):
+    aa = r'''\frac{\text{x1}}{\text{x2}}+\frac{\text{x3}}{\text{x4}}=\frac{\text{x5}}{\text{x6}}'''
+    for ii in zamjene.keys():
+        aa = aa.replace(ii, zamjene[ii])
+    return aa
 
 
-ss = ss.replace('x', str(rjx))
-ss = ss.replace('y', str(rjy))
+keys = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', '+']
+values = ['1','2','3','4', 'brojnik', 'nazivnik', '-']
+zamjene = dict(map(lambda i,j : (i,j) , keys,values))
 
-#with cols[0]:
-st.latex(ss)
+
+
+st.latex(zbroj_razlomaka(zamjene))
