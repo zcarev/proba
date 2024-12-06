fb127d8158104d5061706982d46fe931b4dae47b
zcarev <zdravko.carev@gmail.com>
2024-12-06 21:47:49 +0100

    Update razlomci.py

--- razlomci.py
+++ razlomci.py
@@ -1,2 +1,38 @@
-import streamlit as st
-st.write('Hello word, again!')
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+
+
+ss = r'''\frac{1}{2}+\frac{2}{3}=\frac{x}{y}'''
+
+
+
+st.header('Rješite razlomak:')
+
+cols = st.columns(2)
+
+cols1 = st.columns(2)
+with cols[1]:
+    with cols1[0]:
+        rjx = st.number_input('Unesite brojnik', value=None, step=1)
+        if rjx == 1:
+            st.write('Bravo! - Točan brojnik')
+        else:
+            st.write('Unesite točan odgovor za brojnik')
+
+with cols[1]:
+    with cols1[1]:
+        rjy = st.number_input('Unesite rješenje za y', value=None, step=1)
+        if rjy == 2:
+            st.write('Bravo! - Točan odgovor za y')
+        else:
+            st.write('Unesite točan odgovor za y')
+
+
+ss = ss.replace('x', str(rjx))
+ss = ss.replace('y', str(rjy))
+
+#with cols[0]:
+st.latex(ss)
