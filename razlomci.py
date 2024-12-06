5657708fe37800b1a18666f94594d57aa0f4d69b
zcarev <zdravko.carev@gmail.com>
2024-12-06 21:50:27 +0100

    Update razlomci.py

--- razlomci.py
+++ razlomci.py
@@ -1,15 +1,6 @@
 import streamlit as st
 
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
-
-
 ss = r'''\frac{1}{2}+\frac{2}{3}=\frac{x}{y}'''
-
-
 
 st.header('Rješite razlomak:')
 
